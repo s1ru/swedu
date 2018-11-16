b11dd1cbb020d820fdb50b2444a7d410eb736f5e
Jongwook Kim <jongwook1208@gmail.com>
2018-11-16 10:35:28 +0000

    Jongwook2

--- Node_SW_dir/app.js
+++ Node_SW_dir/app.js
@@ -79,30 +79,8 @@ app.post('/process/search', function(req, res){
     //res.send('Success Application !! ');
 });
 
-app.get('/dynamic', function(req, res){
-  var lis = '';
-  for(var i=0; i<5; i++){
-    lis = lis + '<li>coding</li>';
-  }
-  var time = Date();
-  var output = `
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta charset="utf-8">
-      <title></title>
-    </head>
-    <body>
-        Hello, Dynamic!
-        <ul>
-          ${lis}
-        </ul>
-        ${time}
-    </body>
-  </html>`;
-  res.send(output);
-});
 var router = express.Router();
+/*
 router.route('/process/formResponse').post(function(req, res) {
 	console.log('/process/formResponse 호출됨.');
 
@@ -127,15 +105,7 @@ router.route('/process/formResponse').post(function(req, res) {
 		res.write("<br><br><a href='/process/product'>상품 페이지로 이동하기</a>");
 		res.end();
 	}
-});
-
-
-app.get('/process/', function(req, res){
-    res.send('Hello Router, <img src="/route.png">')
-})
-app.get('/login', function(req, res){
-    res.send('<h1>Login please</h1>');
-});
+}); */
 app.listen(8000, function(){
     console.log('Conneted 8000 port!');
 });
